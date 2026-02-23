510b22309f476a888e3a537c5addb2f5ff7372d2
shiva-teja-sannareddy <shivatejasannareddy@gmail.com>
2026-02-24 03:46:19 +0530

    Initial commit

---
+++ server/poker/src/main/java/in/sannareddy/poker/PokerApplication.java
@@ -0,0 +1,13 @@
+package in.sannareddy.poker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokerApplication.class, args);
+	}
+
+}
---
+++ server/poker/src/test/java/in/sannareddy/poker/PokerApplicationTests.java
@@ -0,0 +1,13 @@
+package in.sannareddy.poker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
